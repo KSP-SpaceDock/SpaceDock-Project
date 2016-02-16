f9cbc60ba68c1bf0bc01277578e2fa03a26a1518
Phillip 'KretschmarSchuldorff' Mayo <cmdjackryan@gmail.com>
2016-02-16 09:26:25 -0500

    The world's ugliest SQL for #7

---
+++ migration_data/user_license_query.sql
@@ -0,0 +1,29 @@
+﻿SELECT 
+  "user"."forumUsername", 
+  "user".id, 
+  mod.user_id, 
+  mod.name,
+  mod.external_link, 
+  mod.license
+FROM 
+  public."user", 
+  public.mod
+WHERE 
+  "user".id = mod.user_id AND
+  "user"."forumUsername" NOT LIKE '' AND
+  mod.license NOT LIKE '%GPL%' AND
+  mod.license NOT LIKE '%MIT%' AND
+  mod.license NOT LIKE '%ublic%omain%' AND
+  mod.license NOT LIKE '%hatever%' AND
+  mod.license NOT LIKE '%BSD%' AND
+  mod.license NOT LIKE '%CC%' AND
+  mod.license NOT LIKE '%GNU%' AND
+  mod.license NOT LIKE '%reative%ommons%' AND
+  mod.license NOT LIKE '%pache%' AND
+  mod.license NOT LIKE '%WTFPL%' AND
+  mod.license NOT LIKE '%SA%' AND
+  mod.license NOT LIKE '%nlicen%e%' AND
+  mod.license NOT LIKE '%ShareAlike%'
+ORDER BY
+  "user".id ASC, 
+  "mod"."license" DESC;
